491d4933a638e7c71db377dfad602bcaa544eb7f
mizukmb <mizukmb6@gmail.com>
2015-05-04 14:18:51 +0900

    generate anime controller and view

---
+++ app/controllers/anime_controller.rb
@@ -0,0 +1,7 @@
+class AnimeController < ApplicationController
+  def index
+  end
+
+  def result
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'anime/index'
+
+  get 'anime/result'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
