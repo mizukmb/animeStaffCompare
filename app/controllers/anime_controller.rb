945e32ed3823f90a266a46b382f077aadcccddf5
mizukmb <mizukmb6@gmail.com>
2015-05-05 11:59:40 +0900

    display text field content

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,5 +1,13 @@
 class AnimeController < ApplicationController
   def index
+    # Controllerでrenderやredirect_toなどで明示的にレンダリングするViewをしていない場合は、
+    # メソッド名のViewファイルがレンダリングされる（例：new => new.html.erb）
+    @anime = Anime.new
+  end
+
+  def search
+    h = params[:anime]
+    render :text => "タイトル : #{h['title']}"
   end
 
   def result
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'anime/index'
-
   get 'anime/result'
+
+  post 'anime/search' => 'anime#search'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
