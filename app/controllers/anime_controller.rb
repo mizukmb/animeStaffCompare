66c0a1c2d21a3312742611de4668789a53ffe6f9
mizukmb <mizukmb6@gmail.com>
2015-05-19 22:19:08 +0900

    scrape anime staff info by .lain(http://lain.gr.jp/mediadb/media/5292)

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 class AnimeController < ApplicationController
   def index
     # Controllerでrenderやredirect_toなどで明示的にレンダリングするViewをしていない場合は、
@@ -11,5 +14,27 @@ class AnimeController < ApplicationController
   end
 
   def result
+  end
+
+  def scrape(num)
+    uri = "http://lain.gr.jp/mediadb/media/#{num}"
+
+    begin
+      res = open(uri)
+    rescue OpenURI::HTTPError => e
+      abort(e.message)
+    end
+
+    hash = Hash.new("none")
+    page = Nokogiri::HTML.parse(res, nil, 'utf-8')
+    page.xpath('/html/body/div[@class="container"]/div[@class="row"]/div[@class="span13"]/div[@id="media-db"]/dl').each do |node|
+      key = node.xpath('./dt')
+      value = node.xpath('./dd')
+      key.length.times do |i|
+        hash.store(key[i].text.to_s, value[i].text.to_s)
+        # p "#{key[i].text.to_s}:#{value[i].text.to_s}"
+      end
+    end
+    return hash
   end
 end
